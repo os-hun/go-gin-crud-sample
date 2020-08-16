736e9ca609aa65cbf871c737ad528ccfbe55d724
Shun <shun12baseball@gmail.com>
2020-08-17 07:50:44 +0900

    create: models/repository/user_psql.go, post_psql.go

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.14
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-gin/gin v0.0.0-20161108173721-bb159f9260a2
 	github.com/jinzhu/gorm v1.9.16
+	github.com/manucorporat/sse v0.0.0-20160126180136-ee05b128a739 // indirect
 )
---
+++ models/repository/post_psql.go
@@ -0,0 +1,74 @@
+package repository
+
+import (
+	"github.com/go-gin/gin"
+	"github.com/os-hun/go-gin-crud-sample/db"
+	"github.com/os-hun/go-gin-crud-sample/models"
+	"github.com/os-hun/go-gin-crud-sample/api"
+)
+
+type PostRepository struct{}
+
+type Post api.Post
+
+// GetAll is get all Post
+func (_ PostRepository) GetAll() ([]Post, error) {
+    db := db.GetDB()
+    var p []Post
+
+    if err := db.Find(&p).Error; err != nil {
+        return nil, err
+    }
+
+    return p, nil
+}
+
+// CreateModel is create Post model
+func (_ PostRepository) CreateModel(p *models.Post) (*models.Post, error) {
+    db := db.GetDB()
+    if err := db.Create(&p).Error; err != nil {
+        return p, err
+    }
+    return p, nil
+}
+
+// GetByID is get a Post by ID
+func (_ PostRepository) GetByID(id int) (models.Post, error) {
+    db := db.GetDB()
+    var p models.Post
+    if err := db.Where("id = ?", id).First(&p).Error; err != nil {
+        return p, err
+    }
+    return p, nil
+}
+
+// UpdateByID is update a Post
+func (_ PostRepository) UpdateByID(id int, c *gin.Context) (api.Post, error) {
+    db := db.GetDB()
+    var p api.Post
+    if err := db.Where("id = ?", id).First(&p).Error; err != nil {
+        return p, err
+    }
+    userID := p.UserID
+    if err := c.BindJSON(&p); err != nil {
+        return p, err
+    }
+    fmt.Printf("%+V", p)
+    p.ID = uint(id)
+    p.UserID = userID
+    db.Save(&p)
+
+    return p, nil
+}
+
+// DeleteByID is delete a Post by ID
+func (_ PostRepository) DeleteByID(id int) error {
+    db := db.GetDB()
+    var p Post
+
+    if err := db.Where("id = ?", id).Delete(&p).Error; err != nil {
+        return err
+    }
+
+    return nil
+}
